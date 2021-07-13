4c7ceab0b08fe9eb1c6af314163dff36dfcec930
gruszkowska <mariana.gru@hotmail.com>
2021-07-13 16:51:53 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://morfoquest.herokuapp.com'),
+    'url' => env('APP_URL', 'http://morfoquest.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
